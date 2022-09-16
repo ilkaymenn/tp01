79cba97eba21d8a3e7d533165da924767d388cdc
ilkaymenn <ilkay.men@edu.univ-fcomte.fr>
2022-09-16 15:13:54 +0200

    formulaire terminer

--- src/components/FormulaireOffreMaison.vue
+++ src/components/FormulaireOffreMaison.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from "@vue/reactivity";
-import Fiche0ffreMaison from "../components/FormulaireOffreMaison.vue";
+import Card from "./card.vue";
+
 // On fait une variable réactive qui réference les données
 // ATTENTION : faire une Ref pas une Reactive car :
 // c'est l'objet qui doit être réactif, pas ses props
@@ -11,14 +12,34 @@ const maison = ref({});
     <div class="p-2">
       <h2 class="text-2xl">Résultat (Prévisualisation)</h2>
       <!-- Optionnel on affiche les données -->
-      <Fiche0ffreMaison v-bind="maison" />
+      <Card v-bind="maison" />
     </div>
     <div class="p-2">
       <!-- On passe la "ref" à FormKit -->
-      <FormKit type="form" v-model="maison">
+      <FormKit
+        type="form"
+        :config="{
+          classes: {
+            input: 'p-1 rounded border-black shadow-sm border',
+            label: 'text-gray-600',
+          },
+        }"
+        v-model="maison"
+        :submit-attrs="{ classes: { input: 'bg-red-300 p-1 rounded' } }"
+      >
         <FormKit name="nom" label="nom" />
         <FormKit name="price" label="prix" type="number" />
-        <FormKit name="favoris" label="mettre en valeur" type="checkbox" />
+        <FormKit
+          name="favoris"
+          label="favoris ?"
+          type="checkbox"
+          wrapper-class="flex"
+        />
+        <FormKit name="txt" label="texte" />
+        <FormKit name="img" label="image" />
+        <FormKit name="nbbed" label="bed" />
+        <FormKit name="nbbath" label="bath" />
+        <FormKit name="nbsize" label="size" />
       </FormKit>
     </div>
   </div>
--- src/pages/edit/new.vue
+++ src/pages/edit/new.vue
@@ -1,12 +1,10 @@
-<template>
-  <div class="p-2">
-    <h1 class="text-2xl">Formulaire</h1>
-    <div>
-      <FormulaireOffreMaison />
-    </div>
-  </div>
-</template>
-
 <script setup lang="ts">
 import FormulaireOffreMaison from "../../components/FormulaireOffreMaison.vue";
 </script>
+
+<template>
+  <div class="p-2">
+    <h1 class="text-2xl">Formulaire</h1>
+    <FormulaireOffreMaison v-bind="maison" />
+  </div>
+</template>
