f5f24c7fc4b98667a25b623ad7b1f0f02dffdf61
ilkaymenn <ilkay.men@edu.univ-fcomte.fr>
2022-09-20 22:34:29 +0200

    liste fetch dupliquer

---
+++ src/pages/liste-fetch copy.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import card from "../components/card.vue";
+
+const res = await fetch("/maisons.json");
+const maisons = await res.json();
+</script>
+
+<template>
+  <div class="p-2">
+    <h1 class="text-2xl">Page Liste Fetch</h1>
+    <div v-for="maison in maisons" :key="maison.nom">
+      <card class="w-1/2" v-bind="maison" />
+    </div>
+  </div>
+</template>
