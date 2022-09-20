939e81fd36fd0c485a7addaeab231f8f80a20028
ilkaymenn <ilkay.men@edu.univ-fcomte.fr>
2022-09-20 22:35:51 +0200

    lien supabase dans app ok

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,11 @@
           <h2>lien vers Page Formulaire</h2>
         </router-link>
       </li>
+      <li>
+        <router-link class="text-red-600 underline" to="/liste-supabase">
+          <h2>lien vers Page Supabase</h2>
+        </router-link>
+      </li>
     </ul>
   </nav>
 
