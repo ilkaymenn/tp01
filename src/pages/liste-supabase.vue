3eaa503c80366a26959732bd1572908c5f81c7ad
ilkaymenn <ilkay.men@edu.univ-fcomte.fr>
2022-09-20 22:38:36 +0200

    lsite supabase renommer + modifier

---
+++ src/pages/liste-supabase.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { supabase } from "../supabase";
+console.log("supabase :", supabase); // pour vérifier et "garder" supabase dans le code
+const maisons = []; // à remplacer par l'appel à Supabase
+</script>
+
+<template>
+  <div class="p-2">
+    <h1 class="text-2xl">Page Liste Supabase</h1>
+    <!-- <div v-for="maison in maisons" :key="maison.nom">
+      <card class="w-1/2" v-bind="maison" />
+    </div> -->
+  </div>
+</template>
