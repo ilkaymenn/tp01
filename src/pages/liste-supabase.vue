a1e54579a9ed4370d81bcb8d91fa8af037ca82a8
ilkaymenn <ilkay.men@edu.univ-fcomte.fr>
2022-09-23 14:04:52 +0200

    supase maison repris dans liste supabase

--- src/pages/liste-supabase.vue
+++ src/pages/liste-supabase.vue
@@ -2,6 +2,8 @@
 import { supabase } from "../supabase";
 console.log("supabase :", supabase); // pour vérifier et "garder" supabase dans le code
 const maisons = []; // à remplacer par l'appel à Supabase
+
+let { data: Maison, error } = await supabase.from("Maison").select("*");
 </script>
 
 <template>
